95c8de61905d303e628219f8fe22bbc300303ffc
Vinicius Coelho <105958570+vCoelh0@users.noreply.github.com>
2023-10-10 23:39:04 -0300

    Desafio Controle de Fluxo
    Desafio realizado no bootcamp Santander da DIO

---
+++ DesafioControleFluxo/src/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+package desafiocontrolefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
